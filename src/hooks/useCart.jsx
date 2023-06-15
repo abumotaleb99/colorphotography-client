fbc68c8c5ce67acddeef6f59762d69c2101dda0e
Abu Motaleb <abumotaleb99@gmail.com>
2023-06-15 17:23:29 +0600

    updated useCart hook

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,21 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import useAxiosSecure from "./useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
-
 const useCart = () => {
   const { user, loading } = useContext(AuthContext);
-  const [axiosSecure] = useAxiosSecure();
+  const token = localStorage.getItem("access-token");
 
   const { refetch, data: cart = [] } = useQuery({
-    queryKey: ["cart", user?.email],
+    queryKey: ["carts", user?.email],
     enabled: !loading,
     queryFn: async () => {
-      const res = await axiosSecure(`/carts?email=${user?.email}`);
-      return res.data;
+      const res = await fetch(
+        `http://localhost:5000/carts?email=${user?.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        }
+      );
+      return res.json();
     },
   });
+
   return [cart, refetch];
 };
-
 export default useCart;
